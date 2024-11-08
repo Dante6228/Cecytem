88cff6f6e3a3043d513717428f1f020f3c176fe2
Dante <dantealejandro35@gmail.com>
2024-11-08 13:02:29 -0600

    Continuación del desarrollo

--- inicio.php
+++ inicio.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/inicio.css">
     <title>Menú principal</title>
 </head>
 <body>
@@ -10,8 +11,10 @@
         <h1>Menú principal</h1>
     </header>
     <main>
-        <a href="crearExamen.php">Crear examen</a>
-
+        <div>
+            <a href="crearExamen.php">Crear examen</a>
+            <a href="cuentas.php">Usuarios</a>
+        </div>
     </main>
 </body>
 </html>
