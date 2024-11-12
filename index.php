eddc57a17ce2fdafac0910c4785cdd4f22515c1e
Dante <dantealejandro35@gmail.com>
2024-11-11 22:40:27 -0600

    adición de css al index

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/general.css">
+
+    <link rel="stylesheet" href="css/login.css">
     <title>Iniciar sesión</title>
 </head>
 <body>
